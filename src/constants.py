8e2768fb952f836cfeec310ed1d33754b7c9c87f
bradenbest <bradentbest@gmail.com>
2014-04-19 14:06:43 -0600

    Fixed sound delay

--- src/constants.py
+++ src/constants.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
   print("This is not supposed to be run directly. To run the game, run python with main.py")
   exit()
 
+pygame.mixer.pre_init(44100,-16,2,512)
 pygame.init()
-pygame.mixer.init()
 
 RESOLUTION = (800,800)
 WINDOW = pygame.display.set_mode(RESOLUTION,0,32) 
