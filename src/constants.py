7006168116db0aea4179700aa0c0bed63840386b
bradenbest <bradentbest@gmail.com>
2014-04-25 19:16:19 -0600

    Added Fullscreen
    Game now plays at fullscreen

--- src/constants.py
+++ src/constants.py
@@ -7,12 +7,18 @@ if __name__ == "__main__":
 pygame.mixer.pre_init(44100,-16,2,512)
 pygame.init()
 
-GRIDSIZE = (60,60)
+
+DISPLAY_INFO = pygame.display.Info()
+RAW_RESOLUTION = (int(DISPLAY_INFO.current_w), int(DISPLAY_INFO.current_h))
 SCALE = 10
 PADDING = 0
-RESOLUTION = (GRIDSIZE[0] * SCALE, GRIDSIZE[1] * SCALE)
+#GRIDSIZE = (60,60)
+GRIDSIZE = (int(RAW_RESOLUTION[0] / SCALE), int(RAW_RESOLUTION[1] / SCALE))
+RESOLUTION = (int(GRIDSIZE[0] * SCALE), int(GRIDSIZE[1] * SCALE))
 WINDOW = pygame.display.set_mode(RESOLUTION,0,32) 
 FPS = 30
+
+pygame.display.toggle_fullscreen()
 
 SIG_NOOP = 0
 SIG_QUIT = 1
